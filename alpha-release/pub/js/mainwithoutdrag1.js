3a0c05cc4d7bdeb34bf4c1f73380dff6ff0a7269
Owen-Ng <s.nigelowennlc@gmail.com>
2020-07-24 20:00:04 -0400

    Delete alpha-release not needed

--- alpha-release/pub/js/mainwithoutdrag1.js
+++
@@ -1,7 +0,0 @@
-"use strict"
-
-const newhead = head([200,500], 100, "orange", false);
-// newhead.addclicklist(['EX1', 'how are you?', 'OKKKKKK!!', 'link withoutdrag.html withoutdrag1','link withoutdrag1.html withoutdrag2'], 100,20);
-newhead.addoubleclick(['withoutdrag.html', 'withoutdrag1.html']);
-
-newhead.addholdclick({'withoutdrag1.html':['top', 'down']}) 
--- alpha-release/pub/main.js
+++
@@ -1,6 +0,0 @@
-"use strict"
-
-const newhead = head([200,500], 100, "img.jpg", true);
-newhead.addclicklist(['Hello', 'how are you', 'OKKKKKK', 'link Screen3.html Screen3', 'link Screen2.html Screen2','link Screen1.html Screen1'], 100,20);
-newhead.addoubleclick(['Screen1.html', 'Screen2.html', 'Screen3.html']);
-newhead.addholdclick({'Screen1.html': ['top', 'down'], 'Screen2.html':['top', 'down'], 'Screen3.html':['top', 'down']}) 
